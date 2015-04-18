cbd5ffa1ecf2c18e4f950a915063bd183b26aa1c
Amr ElGarhy <amrelgarhy81@gmail.com>
2015-04-18 16:03:11 +0100

    Composite Pattern

---
+++ DesignPatterns/Composite1/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite1
+{
+    class Person : IParty
+    {
+        public string Name { get; set; }
+        public int Gold { get; set; }
+
+        public void Stats()
+        {
+            Console.WriteLine("{0} has {1} gold coins", Name, Gold);
+        }
+    }
+}
---
+++ DesignPatterns/Composite1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int goldForKill = 1023;
+            Console.WriteLine("You have killed the giant IE6 Monster and gained {0} gold!", goldForKill);
+
+            var joe = new Person { Name = "Joe" };
+            var jack = new Person { Name = "Jack" };
+            var emily = new Person { Name = "Emily" };
+            var sophia = new Person { Name = "Sophia" };
+            var brain = new Person { Name = "Brain" };
+            var developers = new Group { Name = "Developers", Members = { joe, jack, emily } };
+
+            var parties = new Group { Members = { developers, sophia, brain } };
+
+            parties.Gold += goldForKill;
+            parties.Stats();
+
+            Console.ReadKey();
+        }
+    }
+}
