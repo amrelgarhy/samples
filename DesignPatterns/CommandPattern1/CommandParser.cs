9bb24bbcf52bd3dd6b119d34206fb9d91b42b987
Amr ElGarhy <amrelgarhy81@gmail.com>
2015-05-15 23:19:24 +0100

    Command Pattern

---
+++ DesignPatterns/CommandPattern1/CommandParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandPattern1
+{
+    public class CommandParser
+    {
+        readonly IEnumerable<ICommandFactory> availableCommands;
+
+        public CommandParser(IEnumerable<ICommandFactory> availableCommands)
+        {
+            this.availableCommands = availableCommands;
+        }
+
+        internal ICommand ParseCommand(string[] args)
+        {
+            var requestCommandName = args[0];
+
+            var command = FindRequestedCommand(requestCommandName);
+
+            if (command == null)
+            {
+                return new NotFoundCommand { Name = requestCommandName };
+            }
+            return command.MakeCommand(args);
+        }
+
+        ICommandFactory FindRequestedCommand(string commandName)
+        {
+            return availableCommands
+                .FirstOrDefault(cms => cms.CommandName == commandName);
+        }
+    }
+}
